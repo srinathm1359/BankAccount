9d4feb43cab496142c584cf163bc07f9e7af5ed8
Srinath Mahankali <smahankali10@stuy.edu>
2020-10-28 19:46:00 -0400

    Tested getBalance() and getAccountID()

--- Tester.java
+++ Tester.java
@@ -1,3 +1,9 @@
 public class Tester {
-  
+  public static void main(String[] args) {
+    //Testing getBalance() and getAccountID()
+    BankAccount first_account = new BankAccount(123, "Hello!");
+    System.out.println(first_account.getBalance());
+    System.out.println(first_account.getAccountID());
+  }
+
 }
