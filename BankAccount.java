ed86a4b13d8938fa79269c9891d1f18e7ac8cfc8
Srinath Mahankali <smahankali10@stuy.edu>
2020-10-28 19:39:16 -0400

    Added getBalance()

--- BankAccount.java
+++ BankAccount.java
@@ -2,9 +2,14 @@ public class BankAccount {
   private double balance;
   private int accountID;
   private String password;
+
   public BankAccount(int accountNumber, String pass) {
     accountID = accountNumber;
     password = pass;
     balance = 0;
+  }
+
+  public double getBalance() {
+    return balance;
   }
 }
